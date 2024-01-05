dc72bc03e553b179be8a6bc5b5824ae1b74062b9
nayun <nayunk10@gmail.com>
2024-01-05 19:12:31 +0900

    react 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import {Button, Container, Nav, Navbar, Row, Col} from 'react-bootstrap';
+import { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import data from './data.js'
 
 function App() {
+
+  let [cat] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar data-bs-theme="dark" className='store-nav'>
+        <Container>
+          <Navbar.Brand href="#home" className='nav-title'>세구상점</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link href="#pricing">Mypage</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg' style={{backgroundImage : 'url(./img/gosegu.jpg)'}}></div>
+      <Container className='product-box'>
+        <Row>
+          {
+            cat.map(function(a, i){
+              return <Product cat = {cat[i]} i = {i}></Product>
+            })
+          }
+        </Row>
+      </Container>
     </div>
+  );
+}
+
+function Product(props){
+  return(
+    <Col>
+      <img src={process.env.PUBLIC_URL + "/img/ㄱㅇㅇ" + (props.i + 1) +".jpg"} width="80%"/>
+      <h4>{props.cat.title}</h4>
+      <p>{props.cat.cute}</p>
+    </Col>
   );
 }
 
