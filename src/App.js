d7af417df3bb742a32090e04b2f0c9c191733e6e
nayun <nayunk10@gmail.com>
2024-01-05 20:03:43 +0900

    react 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import {Button, Container, Nav, Navbar, Row, Col} from 'react-bootstrap';
 import { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import data from './data.js'
+import data from './data.js';
+import {Routes, Route, Link} from 'react-router-dom';
+import Detail from './detail.js'
 
 function App() {
 
@@ -10,26 +12,35 @@ function App() {
 
   return (
     <div className="App">
+
       <Navbar data-bs-theme="dark" className='store-nav'>
         <Container>
-          <Navbar.Brand href="#home" className='nav-title'>세구상점</Navbar.Brand>
+          <Navbar.Brand href="home" className='nav-title'>세구상점</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/detail">Cart</Nav.Link>
             <Nav.Link href="#pricing">Mypage</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg' style={{backgroundImage : 'url(./img/gosegu.jpg)'}}></div>
-      <Container className='product-box'>
-        <Row>
-          {
-            cat.map(function(a, i){
-              return <Product cat = {cat[i]} i = {i}></Product>
-            })
-          }
-        </Row>
-      </Container>
+
+      <Routes>
+        {/* 메인페이지 */}
+        <Route path='/' element={<>
+          <div className='main-bg' style={{backgroundImage : 'url(./img/gosegu.jpg)'}}></div>
+          <Container className='product-box'>
+            <Row>
+            {
+              cat.map(function(a, i){
+                return <Product cat = {cat[i]} i = {i}></Product>
+              })
+            }
+            </Row>
+          </Container>
+        </>} />
+        <Route path='/detail' element={<Detail></Detail>} />
+      </Routes>
+      
     </div>
   );
 }
